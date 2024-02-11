3d9fb5f3af6f43bcdaa239f13dc02ade6f0b895b
Alexis Bonetti <alexis.bonetti@gmail.com>
2024-02-11 12:48:08 -0300

    f6

--- SUPERBLOG/SUPERBLOG/wsgi.py
+++ SUPERBLOG/SUPERBLOG/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SUPERBLOG.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SUPERBLOG.SUPERBLOG.settings')
 
 application = get_wsgi_application()
