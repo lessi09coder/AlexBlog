46521a5790b678635229e8fe3ff15ce18d6b3572
Alexis Bonetti <alexis.bonetti@gmail.com>
2024-02-11 12:38:08 -0300

    f4

--- SUPERBLOG/SUPERBLOG/settings.py
+++ SUPERBLOG/SUPERBLOG/settings.py
@@ -35,8 +35,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 # Application definition
 #'render.apps.RenderConfig',
-INSTALLED_APPS = [    
-    'render.apps.RenderConfig'
+INSTALLED_APPS = [        
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
