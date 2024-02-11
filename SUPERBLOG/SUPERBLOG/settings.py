327d6fead617d4b69268a460fa9a208ffd3fafc8
Alexis Bonetti <alexis.bonetti@gmail.com>
2024-02-11 15:15:18 -0300

    123

--- SUPERBLOG/SUPERBLOG/settings.py
+++ SUPERBLOG/SUPERBLOG/settings.py
@@ -133,20 +133,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
