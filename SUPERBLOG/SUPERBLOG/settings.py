e5bc06986b4804012ae718493c21848b3b4eee18
Alexis Bonetti <alexis.bonetti@gmail.com>
2024-02-11 15:54:54 -0300

    ok

--- SUPERBLOG/SUPERBLOG/settings.py
+++ SUPERBLOG/SUPERBLOG/settings.py
@@ -36,7 +36,6 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 #'render.apps.RenderConfig',
 INSTALLED_APPS = [        
-    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -134,18 +133,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-if not DEBUG:
-    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -169,3 +156,11 @@ LOGIN_REDIRECT_URL = 'home'
 MEDIA_URL = 'imagenes/'
 MEDIA_ROOT = BASE_DIR / 'AlexBlog/static/imagenes'
 
+if not DEBUG:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
