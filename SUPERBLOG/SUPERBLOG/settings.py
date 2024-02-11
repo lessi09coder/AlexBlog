fdeb0542533db1f4962ecb801f3e7e7d72a1f437
Alexis Bonetti <alexis.bonetti@gmail.com>
2024-02-11 14:43:09 -0300

    f5

--- SUPERBLOG/SUPERBLOG/settings.py
+++ SUPERBLOG/SUPERBLOG/settings.py
@@ -159,15 +159,7 @@ MEDIA_ROOT = BASE_DIR / 'AlexBlog/static/imagenes'
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATIC_URL = '/static/'
-    STATIC_ROOT = BASE_DIR / 'static'
-    STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'AlexBlog/static')
-    ]
-    STATIC_ROOT = BASE_DIR / 'static'
-    MEDIA_URL = 'imagenes/'
-    MEDIA_ROOT = BASE_DIR / 'AlexBlog/static/imagenes'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
