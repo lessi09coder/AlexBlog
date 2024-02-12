b5d5192619119641d52837e6c6837dccdfbadcfb
Alexis Bonetti <alexis.bonetti@gmail.com>
2024-02-12 10:56:23 -0300

    test5

--- SUPERBLOG/SUPERBLOG/settings.py
+++ SUPERBLOG/SUPERBLOG/settings.py
@@ -152,7 +152,7 @@ STATIC_URL = "/static/"
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_ROOT = os.path.join(BASE_DIR, "AlexBlog/static")
     
     #STATIC_ROOT = BASE_DIR / "/AlexBlog/static/imagenes"
     # Turn on WhiteNoise storage backend that takes care of compressing static files
